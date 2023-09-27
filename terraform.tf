7551d3377f7a4df1dc31a78291ff3e68fb6699e1
Michal Kuritka <kuritka@gmail.com>
2023-09-27 17:50:58 +0200

    remove config_paths

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,6 @@ terraform {
 
   backend "kubernetes" {
     secret_suffix  = "state"
-    config_path    = "~/.kube/config"
     namespace      = "backend"
     config_context = "k3d-west"
   }
@@ -17,7 +16,6 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
   config_context = var.context
   #  terraform plan runs in terraform cloud and doesn't have access to the kubeconfig file
   #  terraform cloud can set terraform variables which can be used to set the kubernetes provider
