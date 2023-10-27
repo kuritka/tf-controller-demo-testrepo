c5a772adc7acfe26e480d8244be838287beaa3b1
Michal Kuritka <kuritka@gmail.com>
2023-10-27 17:32:56 +0200

    version 1.3.0

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.4.0, < 2.0.0"
+  required_version = ">= 1.3.0, < 2.0.0"
 
   backend "kubernetes" {
     # creates default secret name terraform-<workspace-name>-state in backend namespace
